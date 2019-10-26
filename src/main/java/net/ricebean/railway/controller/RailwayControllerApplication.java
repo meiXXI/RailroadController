edbb330303ffcf85fcecbc85ac1ab321264f560c
Stefan Meissner <stefan.meissner@perfectpattern.de>
2019-10-26 22:44:47 +0200

    Initial Commit

---
+++ src/main/java/net/ricebean/railway/controller/RailwayControllerApplication.java
@@ -0,0 +1,13 @@
+package net.ricebean.railway.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ricebean/railway/controller/RailwayControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ricebean.railway.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
